283a39753e5e22d5350357fd8beac58e59a0c3e1
Amin Aslami <101183453+aminaslami@users.noreply.github.com>
2024-10-03 22:35:21 +0300

    Update 2-Evaluate_LLM_Powered_RAG_App_Auto.py

--- 2-Evaluate_LLM_Powered_RAG_App_Auto.py
+++ 2-Evaluate_LLM_Powered_RAG_App_Auto.py
@@ -9,8 +9,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MODEL = "gpt-3.5-turbo"
 
-# ------------------------------------------
-# 2-Step
+# 2-Step ------------------------------------------
 # Scrape the Webiste and Split the Content
 
 from langchain_community.doucment_loaders import WebBaseLoader
@@ -135,23 +134,20 @@ chain = (
     | StrOutputParser()
 )
 
-# ----------------------------------------------
-# 9.1-Step
+# 9.1-Step ----------------------------------------------
 # Let's make sure the chain works by testing it with a simple question
 
 chain.invoke("{question}: What is the Machine Learning School")
 
 
-# ----------------------------------------------
-# 10-Step
+# 10-Step ----------------------------------------------
 # Evaluating the Model on the Test Set
 
 def answer_fn(question, history = None):
     return chain.invoke({"question: question"})
     
     
-# ----------------------------------------------
-# 10.1-Step
+# 10.1-Step ----------------------------------------------
 # We can now use the evaluate() function to evaluate the model Test set.
 # This function will compare the answer from chain with refernce answers in the test set.
 
@@ -160,5 +156,94 @@ report = evaluate(answer_fn, testset = testset, knowledge_base = knowledge_base)
 
 display(report)
    
-# ----------------------------------------------
-# 10.1-Step
+   
+# 11-Step ----------------------------------------------
+
+report.to_html("report.html")   
+
+
+# 12-Step ----------------------------------------------
+# We can display correctness results organized by question type
+
+report.correctness_by_question_type()
+
+
+# 13-Step ----------------------------------------------
+# We can display the specific failures.
+
+report.get_failures()
+
+
+# 14-Step ----------------------------------------------
+# Load the test set from disk.
+
+from giskard.rag import QATestset
+testset = QATestset.load("test-set.jsonl")
+
+
+# 15-Step ----------------------------------------------
+# Create a Test Suite from the test set
+
+test_suite = testset.to_test_suite("Machine Learning School Test Suite")
+
+
+# 16-Step ----------------------------------------------
+# We need a function that takes a DataFrame of questions, invokes the chain with each question, and returns the answers.
+
+import giskard
+
+def batch_prediction_fn(df: pd.DataFrame):
+    return chain.batch([{"question": q} for q in df["question"].values])
+
+
+# 17-Step ----------------------------------------------
+# We can now create a Giskard Model object to run our test suite.
+
+giskard_model = giskard.Model(
+    model = batch_prediction_fn,
+    model_type = "test_generation",
+    name = "Machine Learning School Question and Answer Model",
+    description = "This model answers questions about the Machine Learning School website.",
+    feature_names = ["question"],
+)
+
+
+# 18-Step ----------------------------------------------
+# Let's now run the test suite using the model we created before.
+
+test_suite_reuslts = test_suite.run(model = giskard_model)
+
+
+
+# 19-Step ----------------------------------------------
+# We can display the results
+
+display(test_suite_reuslts)
+
+
+# 20-Step ----------------------------------------------
+# Integrating with Pytest
+
+import ipytest
+
+
+# 21-Step ----------------------------------------------
+# We can now integrate test suit with Pytest.
+
+%%ipytest
+
+import pytest
+from giskard.rag import QATestset
+from giskard.testing.tests.llm import test_llm_correctness
+
+@pytest.fixture
+def dataset():
+    testset = QATestset.load("test-set.jsonl")
+    return testset.to_dataset()
+    
+@pytest.fixture
+def model():
+    return giskard_model
+    
+def test_chain(dataset, model):
+    test_llm_correctness(model = model, dataset = dataset, threashold = 0.5).assert_()
